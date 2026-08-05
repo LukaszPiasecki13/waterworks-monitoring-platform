9a76912e4250c8a7bca46f7a16ca666dc727adc5
Łukasz Piasecki <80245965+LukaszPiasecki13@users.noreply.github.com>
2026-08-05 11:51:58 +0200

    backend: update requirements.txt

--- backend/app/modules/security/api/auth.py
+++ backend/app/modules/security/api/auth.py
@@ -25,8 +25,7 @@ def register(data: RegisterRequest, svc: AuthService = Depends(get_auth_service)
         email=data.email,
         password=data.password,
         first_name=data.first_name,
-        last_name=data.last_name,
-        recruitment_token=data.recruitment_token,
+        last_name=data.last_name
     )
 
 
